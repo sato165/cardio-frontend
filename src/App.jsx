1a24d8bcbf13b920f13c0010ca1406c7e34bbb5d
Sebastian Torres <torreste2002@gamil.com>
2026-04-28 19:57:12 -0500

    mejoras visuales y animaciones

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
+// src/App.jsx
 import { Routes, Route } from 'react-router-dom'
 import Navbar from './components/Navbar'
+import EkgBackground from './components/EkgBackground'
 import HomePage from './pages/HomePage'
 import ManualPage from './pages/ManualPage'
 import UploadPage from './pages/UploadPage'
@@ -7,18 +9,24 @@ import AboutPage from './pages/AboutPage'
 
 export default function App() {
   return (
-    <div className="min-h-screen hero-grid text-slate-200">
-      <Navbar />
-      <main className="pt-20 pb-12 px-4">
-        <div className="max-w-5xl mx-auto">
-          <Routes>
-            <Route path="/" element={<HomePage />} />
-            <Route path="/manual" element={<ManualPage />} />
-            <Route path="/upload" element={<UploadPage />} />
-            <Route path="/about" element={<AboutPage />} />
-          </Routes>
-        </div>
-      </main>
+    <div className="relative min-h-screen hero-grid text-slate-200">
+      {/* Fondo animado fijo (cubre toda la pantalla) */}
+      <EkgBackground color="green" />
+
+      {/* Capa de contenido por encima del fondo */}
+      <div className="relative z-10 flex flex-col min-h-screen">
+        <Navbar />
+        <main className="pt-20 pb-12 px-4 flex-1">
+          <div className="max-w-5xl mx-auto">
+            <Routes>
+              <Route path="/" element={<HomePage />} />
+              <Route path="/manual" element={<ManualPage />} />
+              <Route path="/upload" element={<UploadPage />} />
+              <Route path="/about" element={<AboutPage />} />
+            </Routes>
+          </div>
+        </main>
+      </div>
     </div>
   )
 }
